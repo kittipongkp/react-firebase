e6dae3c5ab0f687e29ea500da3864e5a06219511
unknown <kittipongga@gmail.com>
2019-02-18 20:05:54 +0700

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-
-
-
 import './App.css';
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
   render() {
@@ -12,6 +10,9 @@ class App extends Component {
       <BrowserRouter>
       <div>
         <Navbar />
+        <Switch>
+          <Route path='/' component={Dashboard} />
+        </Switch>
       </div>
       </BrowserRouter>
     );
