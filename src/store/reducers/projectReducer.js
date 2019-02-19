bfe443e063638e8d9012dcfa23a8d1e735bb47a5
unknown <kittipongga@gmail.com>
2019-02-19 15:10:17 +0700

    Project edit

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,10 +1,13 @@
 const initState = {
-    title:"",
-    content:""
+    projects: [
+        {id:'1', title:'help me find peach', content: 'blah blah b a a a '},
+        {id:'2', title:'collect all the start', content: 'blah blah b a a a '},
+        {id:'3', title:'egg hunt with yoshi300', content: 'blah blah b a a a '}
+    ]
 }
 
 const projectReducer = (state = initState, action) =>{
     return state
 }
 
-export default projectReducer
+export default projectReducer 
